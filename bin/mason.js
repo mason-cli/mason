d69f00b245afc65495abaf39102d3bae89f3d91e
Andrew Ewing <drew@phenocode.com>
2017-01-23 23:51:14 -0700

    Use standard node for bin

--- bin/mason.js
+++ bin/mason.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env babel-node
+#!/usr/bin/env node
 
 'use strict'
 
-import Mason from '../lib/index'
-import Arguments from '../lib/cli/Arguments'
+var Mason = require('../lib/index');
+var Arguments = require('../lib/cli/Arguments');
 
 try {
-	let input = new Arguments();
+	var input = new Arguments();
 	Mason.run(input.command(), input.all());
 } catch(e) {
 	console.error('Error: ' + e.message);
