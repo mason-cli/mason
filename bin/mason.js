5c67dce391526db3d1cc9ecdd4b3a9144a97f103
Andrew Ewing <drew@phenocode.com>
2017-07-02 01:58:40 -0700

    Added support for mason.babel.js

--- bin/mason.js
+++ bin/mason.js
@@ -1,42 +1,52 @@
 #!/usr/bin/env node
 
-'use strict'
+"use strict";
 
-require('app-module-path').addPath(process.cwd() + '/node_modules');
+require("app-module-path").addPath(process.cwd() + "/node_modules");
 
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
 
-const Application = require('../lib/cli/Application').default;
-const Input = require('../lib/cli/Input').default;
-const builtins = require('../lib/cli/Builtins').default;
+const Application = require("../lib/cli/Application").default;
+const Input = require("../lib/cli/Input").default;
+const builtins = require("../lib/cli/Builtins").default;
 
 var Mason = new Application();
 builtins(Mason);
 
-const configPath = process.cwd() + '/mason.config.js';
 var config = false;
-if(fs.existsSync(configPath)) {
-	config = require(configPath);
-	if(typeof(config) == 'function') {
+var standardConfig = `${process.cwd()}/mason.config.js`;
+var babelConfig = `${process.cwd()}/mason.babel.js`;
+if (fs.existsSync(standardConfig)) {
+	config = require(standardConfig);
+} else if (fs.existsSync(babelConfig)) {
+	require("babel-register");
+	config = require(babelConfig).default;
+}
+
+if (config) {
+	if (typeof config == "function") {
 		config = config(Mason);
 	}
 	Mason.setConfig(config);
 } else {
-	console.info('WARNING: `mason.config.js` not found');
-	console.log('');
+	console.info("WARNING: Mason config not found");
+	console.log("");
 }
 
 // Load plugins
-if(config.plugins) {
-	config.plugins.forEach((location) => {
-		if(location.substring(0, 2) == './' || location.substring(0, 2) == '.\\') {
+if (config.plugins) {
+	config.plugins.forEach(location => {
+		if (
+			location.substring(0, 2) == "./" ||
+			location.substring(0, 2) == ".\\"
+		) {
 			location = process.cwd() + path.sep + location.substring(2);
 		}
 
 		let plugin = require(location);
-		if(plugin) {
+		if (plugin) {
 			plugin.default(Mason);
 		}
 	});
@@ -45,15 +55,18 @@ if(config.plugins) {
 try {
 	var input = new Input();
 	Mason.run(input.command(), input.all(), config);
-} catch(e) {
-	console.error('Error!', e.message ? e.message : e);
-	if(e.stack) {
+} catch (e) {
+	console.error("Error!", e.message ? e.message : e);
+	if (e.stack) {
 		console.log(util.inspect(e.stack, false, null));
 	}
 }
 
-Mason.finally(() => {
-	// console.log(' ');
-}, (err) => {
-	console.error('Error!', util.inspect(err, false, null));
-});
+Mason.finally(
+	() => {
+		// console.log(' ');
+	},
+	err => {
+		console.error("Error!", util.inspect(err, false, null));
+	}
+);
